1eef220c3ecc6105783d58cf84ee0ecb982b256d
MiriamNM <miriamnavamucino@gmail.com>
2021-04-14 14:08:06 -0500

    process to mdLinks promisse

--- mdLinks.js
+++ mdLinks.js
@@ -19,7 +19,18 @@ const comands = {
   validate: cliCommand[3],
   stats: cliCommand[4],
 }
-console.log('myArgs: ', comands);
+
+//leer archivo INDIVIDUAL.
+const readFile = (file) => {
+  const filesWithRead = fs.readFileSync(file, 'utf8');
+  return filesWithRead;
+}
+
+// Lectura de archivo de carpetas.
+const filesDir = (files) => {
+	const dirWithRead = fs.readdirSync(files, 'utf-8');
+  return dirWithRead;
+}
 
 // Obtener los links.
 const getLinks = (data) => {
@@ -31,15 +42,14 @@ return resultado;
 const validationLinks = (md) => {
   md.forEach((link) => {
     const linkMap = link.replace(/[{()}]/g, '');
-    console.log(linkMap, 'hola')
     fetch(md)
       .then((res) => {
-        console.log(res,'gatito');
-        // if(res.status === 200) {
-        //   console.log({path: path.resolve(linkMap), status: res.status, statusText: 'FAIL', url:linkMap});
-        // } else {
-        //   console.log({path: path.resolve(linkMap), status: res.status, statusText: 'OK', url:linkMap});
-        // }
+      //console.log(res,'gatito');
+        if(res.status === 200) {
+          console.log({path: path.resolve(linkMap), status: res.status, statusText: 'OK', url:linkMap});
+        } else {
+          console.log({path: path.resolve(linkMap), status: res.status, statusText: 'FAIL', url:linkMap});
+        }
       })
       .catch(
         (error) => console.log(error)
@@ -67,21 +77,6 @@ const statsStadistics = (resultado) => {
     console.log(resultado, 'si toy');
     return resultado;
   });
-  
-}
-
-//leer archivo INDIVIDUAL.
-const readFile = (file) => {
-  console.log(file,'es file');
-  const filesWithRead = fs.readFileSync(file, 'utf8');
-  console.log(filesWithRead);
-  return filesWithRead;
-}
-
-// Lectura de archivo de carpetas.
-const filesDir = (files) => {
-	const dirWithRead = fs.readdirSync(files, 'utf-8');
-  return dirWithRead;
 }
 
 // función para poner los archivos md en un array.
@@ -90,7 +85,6 @@ const mds = (filePath) => {
   if (fs.statSync(filePath).isDirectory()) {
     const files = filesDir(filePath);
         files.forEach((file) => {
-          console.log(file);
           if (path.extname(file) === '.md') {
             const mdDir = path.join(filePath,file)
             md= md.concat(mdDir);
@@ -101,43 +95,35 @@ const mds = (filePath) => {
   } else if (path.extname(filePath) === '.md') {
     md= md.concat(filePath); 
   }
-  console.log(md, 'vaca')
   return (md)
 }
 
-const promiseMDLinks = (data) => {
-  return new Promise((resolve, rejects) => {
-  const md = mds(data);
-  md.forEach((fileMd) => {
-    const readMd = readFile(fileMd);
-    const getLinksMd = getLinks(readMd);
-    resolve(getLinksMd);
-  })
-  rejects(error);
-  });
-}  
 // FUNCIÓN DE MDLINKS, NO DEBE IR AQUÍ PERO AUN NO SE MODULAR BIEN.
-MDLinks = (filesPath, options) => {
-  promiseMDLinks(filesPath)
-    .then((getLinksMd) => {
-      if (comands['validate'] === '--v' || comands['validate'] === '--validate') {
-      return Promise.all(validationLinks(getLinksMd));
+const MDLinks = (filePath, comands) => {
+  return new Promise((resolve, rejects) => {
+    const md = mds(filePath);
+    md.forEach((fileMd) => {
+      const readMd = readFile(fileMd);
+      const getLinksMd = getLinks(readMd);
+      if (comands['validate'] === null) {
+        resolve(console.log(getLinksMd));
+      } else {
+        resolve(validationLinks(getLinksMd));
       }
-    })
-    .then((res) => {
-      if (comands['stats'] === '--s' || comands['stats'] === '--stats') {
-        return Promise.all(statsStadistics(res));
-      } 
-    })
-    .then((res) => {console.log(res, 'patito')})
-    .catch(() => {});
+      rejects(error);
     
-  // options('--validate', null);
-  // options('--validate', '--stats');
-  // options(null, '--stats');
-  // options(null, null);   
+    })
+  })   
 }
+
+MDLinks(comands)
+.then((getLinksMd) => {
+    if (comands['stats'] === null) {
+      return Promise.all(console.log(getLinksMd));
+    } else {
+      return Promise.all(statsStadistics(getLinksMd));
+    }
+  })
+  .then((res) => {console.log(res, 'patito')})
+  .catch((err) => {return err});  
 MDLinks(comands['path'],(comands['validate']&&comands['stats']))
-// module.exports = {isFile: isFile};
-// module.exports = {isDir: isDir};
-// module.exports = {searchDir: searchDir};
